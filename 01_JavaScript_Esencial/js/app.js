5b2d6e432aaa66fe749eafd869eca48225eb5251
AXDELAROSA <48331083+AXDELAROSA@users.noreply.github.com>
2023-01-24 11:10:43 -0700

    20230123

--- 01_JavaScript_Esencial/js/app.js
+++ 01_JavaScript_Esencial/js/app.js
@@ -1,1 +1,37 @@
-alert('¡Hola desde un archivo externo!');
+"use strict"
+//  alert('¡Hola desde un archivo externo!');
+//  Este es un mensaje de la consola.
+
+/*  
+    #1 .- Conociendo JavaScript
+
+    console.log('Hola CONSOLA');
+    var valor_pi;
+    valor_pi = "3.1416";
+    var publicData  ='Hola';
+*/
+
+/* 
+    #2 .- Variables
+    var: se recomienda utilizarla para declarar variables globales.
+    let: se recomienda utilizarla para declarar variables en bloque o en una estructura de datos.
+*/
+/*var nombre = 'Alejandro';
+
+console.log(nombre)
+
+function saludo(){
+    let nombre = 'Alex';
+    console.log(nombre)
+    
+    let edad = 34;
+    console.log(edad);
+} 
+
+saludo();
+*/
+
+const valor_pi = 3.1416;
+
+
+valor_pi=15;
