b3a69c0f45893649c7aeeefe3fb99d5c29c74766
AXDELAROSA <48331083+AXDELAROSA@users.noreply.github.com>
2023-01-25 14:19:44 -0700

    Update app.js

--- 01_JavaScript_Esencial/js/app.js
+++ 01_JavaScript_Esencial/js/app.js
@@ -31,7 +31,45 @@ function saludo(){
 saludo();
 */
 
+/*  #3 .- Constantes: Const es la palabra reservada para declarar una variable con valor constante.
 const valor_pi = 3.1416;
 
+// valor_pi=15;
+*/
 
-valor_pi=15;
+/*  #4 .- Números: 
+
+var edad = 35;
+
+var cantidad = "100"; //variable declarada con comillas la toma como tipo Texto.
+
+var nuevaCantidad = Number(cantidad);
+
+parseInt();
+parseFloat();
+*/
+
+/*  #5 .- Cádenas de Texto  
+
+var bebida = "agua";
+
+var comida = 'ceviche';
+
+var instruccion = "El platillo se llama 'ceviche' ";
+
+var edad = 34;
+
+var edadTexto = String(edad);
+*/
+
+/*  #6  .- Booleanos
+var activo = false;
+
+var estado = Boolean(10<9);
+
+/*  # 7 .- Fechas  
+var fecha = new Date();
+*/
+
+
+
