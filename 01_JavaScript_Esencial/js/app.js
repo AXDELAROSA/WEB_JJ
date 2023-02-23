e3f499259d39b64d3a15979a0382c5ec6277f8a6
AXDELAROSA <48331083+AXDELAROSA@users.noreply.github.com>
2023-02-23 08:15:08 -0700

    20230222

--- 01_JavaScript_Esencial/js/app.js
+++ 01_JavaScript_Esencial/js/app.js
@@ -1228,3 +1228,84 @@ resultado = menu.every( platillo => platillo.precio <= 60);
 console.log('¿Todos los platillos cuestan menos de 10? ', resultado);
 */
 
+//********************************
+//*** Obteniendo datos con fetch
+/*
+var boton = document.getElementById('boton');
+var contenedor = document.getElementById('contenedor');
+var posts = null;
+
+boton.addEventListener('click', function () {
+    fetch('http://jsonplaceholder.typicode.com/posts')
+    .then(data => data.json())
+    .then(data => {
+        posts = data;
+        mostrarDatos(posts)
+    })
+});
+
+function mostrarDatos(posts) {
+    posts.map((post, i) => {
+        let titulo = document.createElement('h1');
+        let contenido = document.createElement('p');
+
+        titulo.innerHTML = (i + 1) + " - " + post.title;
+        contenido.innerHTML = post.body;
+
+        contenedor.appendChild(titulo);
+        contenedor.appendChild(contenido);
+    })
+}*/
+
+//********************************
+//*** Trabajando con promesas
+
+var boton = document.getElementById('boton');
+var contenedor = document.getElementById('contenedor');
+var contBanderas = document.getElementById('banderas');
+
+boton.addEventListener('click', function () {
+    getPosts()
+        .then(data => data.json())
+        .then(posts => {
+            mostrarDatos(posts);
+            return getCountries();
+        })
+        .then(data => data.json())
+        .then(countries => {
+            mostrarBanderas(countries);
+        });
+});
+
+function getPosts() {
+    return fetch('http://jsonplaceholder.typicode.com/posts');
+}
+
+function getCountries() {
+    return fetch('https://restcountries.com/v3.1/all');
+}
+
+function mostrarBanderas(countries) {
+    contBanderas.innerHTML = '';
+    countries.map((country, i) => {
+        let bandera = document.createElement('png');
+        bandera.src = country.flags;
+        bandera.width = '20';
+        bandera.height = '20';
+        contBanderas.appendChild(bandera);
+    })
+}
+
+function mostrarDatos(posts) {
+    contBanderas.innerHTML = '';
+    posts.map((post, i) => {
+        let titulo = document.createElement('h1');
+        let contenido = document.createElement('p');
+
+        titulo.innerHTML = (i + 1) + " - " + post.title;
+        contenido.innerHTML = post.body;
+
+        contenedor.appendChild(titulo);
+        contenedor.appendChild(contenido);
+    })
+}
