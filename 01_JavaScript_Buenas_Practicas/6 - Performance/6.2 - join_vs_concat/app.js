fd2c465f5a0bd733ef0287763707064ab9aabc83
AXDELAROSA <48331083+AXDELAROSA@users.noreply.github.com>
2023-10-12 08:41:49 -0600

    Update app.js

--- 01_JavaScript_Buenas_Practicas/6 - Performance/6.2 - join_vs_concat/app.js
+++ 01_JavaScript_Buenas_Practicas/6 - Performance/6.2 - join_vs_concat/app.js
@@ -4,7 +4,7 @@ var listaAnimales = animales.join(", ");
 
 var nuevaLista = animales.concat(",");
 
-console.log(listaAnimales);
+ console.log(listaAnimales);
 console.log(typeof listaAnimales);
 
 console.log(nuevaLista);
